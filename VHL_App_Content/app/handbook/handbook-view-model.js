0414c0506b385dbdbd79cfc64cc36b2c84a468c4
Austin Trivett <atrivett@umass.edu>
2017-12-16 19:52:53 -0500

    testing list view in app

--- VHL_App_Content/app/handbook/handbook-view-model.js
+++ VHL_App_Content/app/handbook/handbook-view-model.js
@@ -2,7 +2,25 @@ const observableModule = require("data/observable");
 
 function HandbookViewModel() {
     const viewModel = observableModule.fromObject({
-
+        countries: [
+            { name: "Australia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/au.png" },
+            { name: "Belgium", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/be.png" },
+            { name: "Bulgaria", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/bg.png" },
+            { name: "Canada", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ca.png" },
+            { name: "Switzerland", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ch.png" },
+            { name: "China", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/cn.png" },
+            { name: "Czech Republic", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/cz.png" },
+            { name: "Germany", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/de.png" },
+            { name: "Spain", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/es.png" },
+            { name: "Ethiopia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/et.png" },
+            { name: "Croatia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hr.png" },
+            { name: "Hungary", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hu.png" },
+            { name: "Italy", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/it.png" },
+            { name: "Jamaica", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/jm.png" },
+            { name: "Romania", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ro.png" },
+            { name: "Russia", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/ru.png" },
+            { name: "United States", imageSrc: "https://play.nativescript.org/dist/assets/img/flags/us.png" },
+        ],
     });
 
     return viewModel;
