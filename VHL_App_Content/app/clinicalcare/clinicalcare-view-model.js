a2151ae156ac11741b5c014e86596ba3ed8a02b1
qsmalljr <qmanjr123@gmail.com>
2017-12-17 19:23:34 -0500

    clinical care template set

--- VHL_App_Content/app/clinicalcare/clinicalcare-view-model.js
+++ VHL_App_Content/app/clinicalcare/clinicalcare-view-model.js
@@ -4,13 +4,13 @@ var utilsModule = require("tns-core-modules/utils/utils");
 function ClinicalCareModel() {
     const viewModel = observableModule.fromObject({
         sections: [
-            { name: "CALIFORNIA", imageSrc: "1" },
-            { name: "Possible VHL Manifestations", imageSrc: "2" },
-            { name: "Healthy Living for the VHL Family", imageSrc: "3" },
-            { name: "Discussing VHL with Your Family", imageSrc: "4" },
-            { name: "VHL Research", imageSrc: "5" },
-            { name: "Glossary of Medical Terms", imageSrc: "6" },
-            { name: "VHL Support Resources", imageSrc: "7" },,
+            { name: "CALIFORNIA", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "Contact: Vee Vo, Clinic Coordinator", tel: "Tel: 650.736.0262; Fax: 650.495.6813", email: "E-mail: veevo@stanfordhealthcare.org", web: "Click for Website", adult: "ADULT ONLY"},
+            { name: "Possible VHL Manifestations", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "", tel: "", email: "", web: "Click for Website", adult: "" },
+            { name: "Healthy Living for the VHL Family", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "", tel: "", email: "", web: "Click for Website", adult: "" },
+            { name: "Discussing VHL with Your Family", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "", tel: "", email: "", web: "Click for Website", adult: "" },
+            { name: "VHL Research", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "", tel: "", email: "", web: "Click for Website", adult: "" },
+            { name: "Glossary of Medical Terms", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "", tel: "", email: "", web: "Click for Website", adult: "" },
+            { name: "VHL Support Resources", desc: "Stanford Hospital and Clinics/Stanford University School of Medicine" , contact: "", tel: "", email: "", web: "Click for Website", adult: "" },,
         ],
 
         onButtonTap: function () {
