42cc2f76fbfed8c6703b642a047db63405d4beb9
Austin Trivett <atrivett@umass.edu>
2017-12-17 21:36:21 -0500

    sss

--- VHL_App_Content/app/faq/faq-view-model.js
+++ VHL_App_Content/app/faq/faq-view-model.js
@@ -1,8 +1,4 @@
 const observableModule = require("data/observable");
-import * as fs from "tns-core-modules/file-system";
-var documents = fs.knownFolders.documents();
-var path = fs.path.join(documents.path, "CompleteHandbook.pdf");
-var file = fs.File.fromPath(path);
 
 function FAQModel() {
     const viewModel = observableModule.fromObject({
