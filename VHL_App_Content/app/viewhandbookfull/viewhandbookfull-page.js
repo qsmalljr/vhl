2f48fabba7a938a85fbbb0c0179c5de3293ab667
Austin Trivett <atrivett@umass.edu>
2017-12-18 18:49:12 -0500

    quick

--- VHL_App_Content/app/viewhandbookfull/viewhandbookfull-page.js
+++ VHL_App_Content/app/viewhandbookfull/viewhandbookfull-page.js
@@ -19,6 +19,11 @@ function onNavigatingTo(args) {
     page.bindingContext = new ViewHandbookFullViewModel();
 }
 
+exports.pageLoaded = function (args) {
+    var page = args.object;
+    page.bindingContext = {};
+}
+
 /* ***********************************************************
  * According to guidelines, if you have a drawer on your page, you should always
  * have a button that opens it. Get a reference to the RadSideDrawer view and
