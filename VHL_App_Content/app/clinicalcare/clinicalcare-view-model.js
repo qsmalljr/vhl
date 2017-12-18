9f34edcf5bd9c40d8f6820161dc8f8d3aeb58cad
qsmalljr <qmanjr123@gmail.com>
2017-12-17 19:11:41 -0500

    clinical care is going

--- VHL_App_Content/app/clinicalcare/clinicalcare-view-model.js
+++ VHL_App_Content/app/clinicalcare/clinicalcare-view-model.js
@@ -1,8 +1,22 @@
 const observableModule = require("data/observable");
+var utilsModule = require("tns-core-modules/utils/utils");
 
 function ClinicalCareModel() {
     const viewModel = observableModule.fromObject({
+        sections: [
+            { name: "CALIFORNIA", imageSrc: "1" },
+            { name: "Possible VHL Manifestations", imageSrc: "2" },
+            { name: "Healthy Living for the VHL Family", imageSrc: "3" },
+            { name: "Discussing VHL with Your Family", imageSrc: "4" },
+            { name: "VHL Research", imageSrc: "5" },
+            { name: "Glossary of Medical Terms", imageSrc: "6" },
+            { name: "VHL Support Resources", imageSrc: "7" },,
+        ],
 
+        onButtonTap: function () {
+            utilsModule.openUrl("https://google.com")
+        },
+	
     });
 
     return viewModel;
