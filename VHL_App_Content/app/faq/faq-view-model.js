1f2d1ff5e55b66e3979a899859f12ab36bdd4bfa
Austin Trivett <atrivett@umass.edu>
2017-12-18 22:08:36 -0500

    xd

--- VHL_App_Content/app/faq/faq-view-model.js
+++ VHL_App_Content/app/faq/faq-view-model.js
@@ -8,7 +8,7 @@ function FAQModel() {
                 title: "1", footer: "10", headerText: "First", footerText: "4",
                 items: [
                     { image: "~/images/a9ff17db85f8136619feb0d5a200c0e4.png", text: "Stop" },
-                    { text: "Drop", image: "http://static.srcdn.com/wp-content/uploads/Superman-fighting-Goku.jpg" }
+                    { text: "Drop", image: "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_l5pcusxy/def_height/1024/def_width/1956/version/100011/type/2/q/100" }
                 ]
             }
         ]
