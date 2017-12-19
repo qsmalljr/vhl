7f002e51a293790dac2b1daf287a97b564ad58b5
Austin Trivett <atrivett@umass.edu>
2017-12-18 22:24:38 -0500

    accordion test

--- VHL_App_Content/app/faq/faq-view-model.js
+++ VHL_App_Content/app/faq/faq-view-model.js
@@ -3,15 +3,16 @@ const observableModule = require("data/observable");
 function FAQModel() {
     const viewModel = observableModule.fromObject({
 
-        bigitems: [
-            {
-                title: "1", footer: "10", headerText: "First", footerText: "4",
+                myitems: [
+                    { title: "Quentin", footer: "Austin", headerText: "First", footerText: "4" },
+                ],
+
                 items: [
                     { image: "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_l5pcusxy/def_height/1024/def_width/1956/version/100011/type/2/q/100", text: "Stop" },
                     { text: "Drop", image: "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_l5pcusxy/def_height/1024/def_width/1956/version/100011/type/2/q/100" }
                 ]
-            }
-        ]
+            
+
 
     });
 
