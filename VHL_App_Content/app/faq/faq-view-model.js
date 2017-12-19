e03f7ff701764f6333be02d63e349a2863e1bd58
Austin Trivett <atrivett@umass.edu>
2017-12-18 22:25:57 -0500

    d

--- VHL_App_Content/app/faq/faq-view-model.js
+++ VHL_App_Content/app/faq/faq-view-model.js
@@ -10,7 +10,7 @@ function FAQModel() {
                 items: [
                     { image: "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_l5pcusxy/def_height/1024/def_width/1956/version/100011/type/2/q/100", text: "Stop" },
                     { text: "Drop", image: "https://prnewswire2-a.akamaihd.net/p/1893751/sp/189375100/thumbnail/entry_id/1_l5pcusxy/def_height/1024/def_width/1956/version/100011/type/2/q/100" }
-                ]
+                ],
             
 
 
