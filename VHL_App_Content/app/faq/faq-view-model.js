9166bbe54b9d0c9a2d78176bf087cf113d8c4478
Austin Trivett <atrivett@umass.edu>
2017-12-18 22:02:47 -0500

    accordion test

--- VHL_App_Content/app/faq/faq-view-model.js
+++ VHL_App_Content/app/faq/faq-view-model.js
@@ -3,6 +3,16 @@ const observableModule = require("data/observable");
 function FAQModel() {
     const viewModel = observableModule.fromObject({
 
+        this.items = [
+            {
+                title: "1", footer: "10", headerText: "First", footerText: "4",
+                items: [
+                    { image: "~/images/a9ff17db85f8136619feb0d5a200c0e4.png", text: "Stop" },
+                    { text: "Drop", image: "http://static.srcdn.com/wp-content/uploads/Superman-fighting-Goku.jpg" }
+                ]
+            }
+        ]
+
     });
 
     return viewModel;
