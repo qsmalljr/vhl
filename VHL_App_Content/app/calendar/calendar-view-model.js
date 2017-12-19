1736885ae37564c672b3b9d4ca0c38187c471eb7
Austin Trivett <atrivett@umass.edu>
2017-12-18 21:30:52 -0500

    big commity dities

---
+++ VHL_App_Content/app/calendar/calendar-view-model.js
@@ -0,0 +1,11 @@
+const observableModule = require("data/observable");
+
+function CalendarViewModel() {
+    const viewModel = observableModule.fromObject({
+
+    });
+
+    return viewModel;
+}
+
+module.exports = CalendarViewModel;
---
+++ VHL_App_Content/app/ercard/ercard-view-model.js
@@ -0,0 +1,11 @@
+const observableModule = require("data/observable");
+
+function ERCardViewModel() {
+    const viewModel = observableModule.fromObject({
+
+    });
+
+    return viewModel;
+}
+
+module.exports = ERCardViewModel;
