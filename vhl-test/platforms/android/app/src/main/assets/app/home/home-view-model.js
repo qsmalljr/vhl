71c475afc73c4aecefcef3e0ae9f478aae20f212
qsmalljr <qmanjr123@gmail.com>
2017-12-22 15:45:16 -0500

    clinical care finished

--- vhl-test/platforms/android/app/src/main/assets/app/home/home-view-model.js
+++ vhl-test/platforms/android/app/src/main/assets/app/home/home-view-model.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var observable_1 = require("data/observable");
+var utilsModule = require("tns-core-modules/utils/utils");
 var HomeViewModel = /** @class */ (function (_super) {
     __extends(HomeViewModel, _super);
     function HomeViewModel() {
-        return _super.call(this) || this;
+        var _this = _super.call(this) || this;
+        _this.donate = function () {
+            utilsModule.openUrl("https://donatenow.networkforgood.org/1411829?code=orange");
+        };
+        return _this;
     }
     return HomeViewModel;
 }(observable_1.Observable));
 exports.HomeViewModel = HomeViewModel;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9tZS12aWV3LW1vZGVsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiaG9tZS12aWV3LW1vZGVsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsOENBQTZDO0FBRTdDO0lBQW1DLGlDQUFVO0lBQ3pDO2VBQ0ksaUJBQU87SUFDWCxDQUFDO0lBQ0wsb0JBQUM7QUFBRCxDQUFDLEFBSkQsQ0FBbUMsdUJBQVUsR0FJNUM7QUFKWSxzQ0FBYSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE9ic2VydmFibGUgfSBmcm9tIFwiZGF0YS9vYnNlcnZhYmxlXCI7XG5cbmV4cG9ydCBjbGFzcyBIb21lVmlld01vZGVsIGV4dGVuZHMgT2JzZXJ2YWJsZSB7XG4gICAgY29uc3RydWN0b3IoKSB7XG4gICAgICAgIHN1cGVyKCk7XG4gICAgfVxufVxuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaG9tZS12aWV3LW1vZGVsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiaG9tZS12aWV3LW1vZGVsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsOENBQTZDO0FBQzdDLElBQUksV0FBVyxHQUFHLE9BQU8sQ0FBQyw4QkFBOEIsQ0FBQyxDQUFDO0FBRTFEO0lBQW1DLGlDQUFVO0lBQ3pDO1FBQUEsWUFDSSxpQkFBTyxTQUNWO1FBRUcsWUFBTSxHQUFHO1lBQ2YsV0FBVyxDQUFDLE9BQU8sQ0FBQywwREFBMEQsQ0FBQyxDQUFBO1FBRWhGLENBQUMsQ0FBQTs7SUFMRSxDQUFDO0lBT0wsb0JBQUM7QUFBRCxDQUFDLEFBVkQsQ0FBbUMsdUJBQVUsR0FVNUM7QUFWWSxzQ0FBYSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE9ic2VydmFibGUgfSBmcm9tIFwiZGF0YS9vYnNlcnZhYmxlXCI7XG52YXIgdXRpbHNNb2R1bGUgPSByZXF1aXJlKFwidG5zLWNvcmUtbW9kdWxlcy91dGlscy91dGlsc1wiKTtcblxuZXhwb3J0IGNsYXNzIEhvbWVWaWV3TW9kZWwgZXh0ZW5kcyBPYnNlcnZhYmxlIHtcbiAgICBjb25zdHJ1Y3RvcigpIHtcbiAgICAgICAgc3VwZXIoKTtcbiAgICB9XG5cdFxuXHRwdWJsaWMgZG9uYXRlID0gZnVuY3Rpb24gKCkge1xuXHRcdHV0aWxzTW9kdWxlLm9wZW5VcmwoXCJodHRwczovL2RvbmF0ZW5vdy5uZXR3b3JrZm9yZ29vZC5vcmcvMTQxMTgyOT9jb2RlPW9yYW5nZVwiKVxuXG5cdH1cblx0XG59XG4iXX0=
