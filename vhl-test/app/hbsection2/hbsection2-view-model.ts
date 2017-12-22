cea3052b36168d82dad8694c731878cd5575f4af
Austin Trivett <atrivett@umass.edu>
2017-12-22 16:29:05 -0500

    all of the handbook sections

---
+++ vhl-test/app/hbsection2/hbsection2-view-model.ts
@@ -0,0 +1,7 @@
+import { Observable } from "data/observable";
+
+export class hbsection2ViewModel extends Observable {
+    constructor() {
+        super();
+    }
+}
---
+++ vhl-test/app/hbsection3/hbsection3-view-model.ts
@@ -0,0 +1,7 @@
+import { Observable } from "data/observable";
+
+export class hbsection3ViewModel extends Observable {
+    constructor() {
+        super();
+    }
+}
