0d942c065be4df87ce7c78b8dd02bce8b021f44b
Austin Trivett <atrivett@umass.edu>
2017-12-22 16:38:02 -0500

    faq handboko

--- vhl-test/app/handbook/handbook-view-model.ts
+++ vhl-test/app/handbook/handbook-view-model.ts
@@ -4,4 +4,9 @@ export class handbookViewModel extends Observable {
     constructor() {
         super();
     }
+    public tapAction(args) {
+		//ADD NAVIGATION CODE HERE
+		frameModule.topmost().navigate("hbsection" + (args.index + 1) + "/hbsection" + (args.index + 1));	
+	}
+	
 }
